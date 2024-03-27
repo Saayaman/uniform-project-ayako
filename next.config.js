e74e17b9867e5e36674d0f32f30930bbc03e9b76
saayaman <ayako@fournine.digital>
2024-03-27 14:44:36 -0700

    update package

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '*' }],
     deviceSizes: [320, 420, 640, 768, 1024, 1280, 1536],
-  },
+  }
 };
 
 module.exports = nextConfig;
