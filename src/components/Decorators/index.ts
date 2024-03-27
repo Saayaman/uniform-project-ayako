14c998c59ce3312494221fba729268faf7bd7072
saayaman <ayako@fournine.digital>
2024-03-27 10:08:07 -0700

    update

--- src/components/Decorators/index.ts
+++ src/components/Decorators/index.ts
@@ -1,12 +1,12 @@
 import { BackgroundDecorator, WithoutContainerDecorator } from '../BasePage';
-import { AccordionItemDecorator } from '../../modules/canvas/AccordionItem';
+// import { AccordionItemDecorator } from '../../modules/canvas/AccordionItem';
 import { CardDecorator } from '../../modules/canvas/Card';
 import { FeatureDecorator } from '../../modules/canvas/Feature';
 
 // Basic Decorators Set for the Component Starter Kit
 // Activate visual editing doc: https://docs.uniform.app/docs/guides/composition/visual-editing/activate-visual-editing
 const CSKDecorators = [
-  AccordionItemDecorator,
+  // AccordionItemDecorator,
   CardDecorator,
   FeatureDecorator,
   WithoutContainerDecorator,
